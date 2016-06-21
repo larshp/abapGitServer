13fb6dd3e12b4e5f16039b1a49c2b439d73a6af1
larshp <larshp@hotmail.com>
2016-06-21 12:38:51 +0000

    fix basename

--- src/frontend/02b363142b451ed68df0b5608910d699.smim.script.js
+++ src/frontend/02b363142b451ed68df0b5608910d699.smim.script.js
@@ -1,12 +1,14 @@
+const Link = ReactRouter.Link;
+
 class REST {
   listRepositories() {
-// http://52.208.177.144:50000/sap/zgit/rest/repositories/    
+// http://ip:50000/sap/zgit/rest/repositories/    
   }
 }
 
 class NoMatch extends React.Component {
   render() {
-    return (<h1>router, no match {this.props.params.id}</h1>);
+    return (<h1>router, no match</h1>);
   }
 }
             
@@ -14,7 +16,7 @@ class RepoList extends React.Component {
   render() {
     return (
       <div><h1>RepoList</h1>
-      <ReactRouter.Link to="/sap/zgit/foobar/">foobar</ReactRouter.Link>
+      <Link to="foobar/">foobar</Link>
       </div>);
   }
 }
@@ -29,22 +31,23 @@ class Repo extends React.Component {
   }
 }                  
             
+      
 class Router extends React.Component {
-  render() {
+        
+  render() { 
+    const history = ReactRouter.useRouterHistory(History.createHistory)({
+      basename: '/sap/zgit'
+      });
       
     return (
-      <ReactRouter.Router history={ReactRouter.browserHistory} >
+      <ReactRouter.Router history={history} >
         <ReactRouter.Route path="/">
-          <ReactRouter.Route path="sap">
-            <ReactRouter.Route path="zgit">
-              <ReactRouter.IndexRoute component={RepoList} />
-              <ReactRouter.Route path=":id" component={Repo} />
-            </ReactRouter.Route>
-          </ReactRouter.Route>
+          <ReactRouter.IndexRoute component={RepoList} />
+          <ReactRouter.Route path=":id" component={Repo} />
         </ReactRouter.Route>
-        <ReactRouter.Route path=":path" component={NoMatch} />
+        <ReactRouter.Route path="*" component={NoMatch} />
       </ReactRouter.Router>);
   }
 }
-
+      
 ReactDOM.render(<Router />, document.getElementById('app'));
