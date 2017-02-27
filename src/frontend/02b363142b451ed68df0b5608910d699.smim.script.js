369764bee272a4ba4cc84a23851db80d820a496e
larshp <larshp@hotmail.com>
2017-02-27 08:20:10 +0100

    fix logo
    did not work for all url possibilities

--- src/frontend/02b363142b451ed68df0b5608910d699.smim.script.js
+++ src/frontend/02b363142b451ed68df0b5608910d699.smim.script.js
@@ -752,7 +752,7 @@ class RepoList extends React.Component {
       <a href="https://github.com/larshp/abapGitServer"><img className="topright" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" /></a>
       <table>
       <tr><td>
-      <img src="./static/logo.svg" height="100"></img>
+      <img src={base + "/static/logo.svg"} height="100"></img>
       </td><td>
       <h1>abapGitServer</h1>
       </td></tr>
