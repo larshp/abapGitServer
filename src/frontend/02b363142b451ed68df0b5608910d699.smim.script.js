ef8ff4c14fcf2449e5b2af1769f627c8fcd71125
larshp <larshp@hotmail.com>
2017-07-23 11:13:08 +0000

    add clone url, close #60

--- src/frontend/02b363142b451ed68df0b5608910d699.smim.script.js
+++ src/frontend/02b363142b451ed68df0b5608910d699.smim.script.js
@@ -28,7 +28,15 @@ class Octicons {
             
   static pencil() {
     return (<span className="octicon octicon-pencil"></span>);
-  }               
+  }   
+
+  static clippy() {
+    return (<span className="octicon octicon-clippy"></span>);
+  }            
+}
+
+function cloneURL(repo) {
+    return window.location.origin + base + "/git/" + repo + ".git";
 }
 
 class Time {
@@ -631,12 +639,10 @@ class BranchList extends React.Component {
   }
   
   render() {
-    let clone = window.location.origin + base + "/git/" + this.props.params.repo + ".git";
-      
     return(<div>
       <Breadcrumb routes={this.props.routes} params={this.props.params} />
       <h1>Branch list</h1>
-      Clone URL: {clone}<br />
+      Clone URL: {cloneURL(this.props.params.repo)}<br />
       <br />
       {this.state.spinner?<Spinner />:this.list()}                      
       </div>);
@@ -844,7 +850,7 @@ class RepoList extends React.Component {
       <a href="https://github.com/larshp/abapGitServer"><img className="topright" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" /></a>
       <table>
       <tr><td>
-      <img src={base + "/static/logo.svg"} height="100"></img>
+      <a href="./"><img src={base + "/static/logo.svg"} height="100"></img></a>
       </td><td>
       <h1>abapGitServer</h1>
       </td></tr>
@@ -853,6 +859,8 @@ class RepoList extends React.Component {
       <br />
       {Octicons.plus()} <Link to="/create">Create</Link>
       <br />
+      <br />
+      {Octicons.clippy()} {cloneURL("<name>")}
       <br />
       <p className="right">
       <a href="/sap/zabapgitserver/rest/swagger.html"><img src={base + "/static/swagger.png"} height="50"></img></a>
